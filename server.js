1a4a1ea349424c7d375be07f422870efef973139
Dishan Nahtiya <dishan@WIN2.CS.KSU.EDU>
2017-01-31 14:29:13 -0600

    somthing wrong with the style

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var http = require('http');
 var fs =  require('fs');
 var port = 3001;
 
+var stylesheet = fs.readFileSync('gallery.css');
+
+var imageNames = ['ace.jpg','bubble.jpg','chess.jpg'];
 
 var server = http.createServer(function(req, res){
 	
@@ -33,15 +36,38 @@ function serveImage(filename,req,res){
 }	
 	
 switch(req.url){
-		
+	case "/gallery":
+		//res.end("Hello.Time is "+ Date.now());
+var ghtml = imageNames.map(function(fileName){
+	'<img src="images/' +fileName+' alt="a fishing ace at work" >'
+});	
+	var html = '<!doctype html>';
+		html += '<head><title>Dishan Gallery</title></head>';
+		//html += 'Hello.Time is '+ Date.now();
+		html += '<link href="gallery.css" rel="stylesheet" type="text/css" >';
+		html += '<body>';
+		html += '<h1>Gallery</h1>';
+		//html += '<img src="images/ace.jpg" alt="a fishing ace at work" >';
+		html += ghtml;
+		html += '</body>';
+		res.setHeader('content-Tyep','text/html');
+		res.end(html);
+		break;
 	case "/chess":
 		serveImage('chess.jpg',req,res);
 		break;
+	case"/gallery.css":
+		res.setHeader('Content-Tyep','text/css');
+		res.end(stylesheet);
 	case"/fern":	
 	case"/fern/":
 	case"/fern.jpg/":
 	case"/fern.jpeg/":
 		serveImage('fern.jpg',req,res);
+		break;
+	case"/ace":
+		serveImage('ace.jpg',req,res);
+		break;
 	default:
 		res.statusCode = 404;
 		res.statusMessage = "Not found";

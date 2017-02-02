1b132082a250bf0c141843e624993991f9fdaf7f
Dishan Nahtiya <dishan@WIN2.CS.KSU.EDU>
2017-02-02 14:21:30 -0600

    2/2/2017 done

--- server.js
+++ server.js
@@ -16,15 +16,56 @@ var stylesheet = fs.readFileSync('gallery.css');
 
 var imageNames = ['/ace','/bubble','/chess','/fern','/mobile'];
 
-
+function getImageNames(callback){
+	fs.readdir('images',function(err,fileNames){ // synchro function
+		if(err)callback(err,undefined);
+			else callback(false,fileNames);
+	});
 	
+}
+
+function imageNamesToTags(fileNames){
+	return fileNames.map(function(fileName){
+			return ' <img src="'+fileName+'" alt="'+fileName+'">';
+	});
+	
+}
+	
+function buildGallery(imageTags){
+	var html = '<!doctype html>';
+		html += '<head><title>Dishan Gallery</title>';
+		//html += 'Hello.Time is '+ Date.now();
+		html += '<link href="gallery.css" rel="stylesheet" type="text/css" >';
+		html += '</head>'; 
+		html += '<body>';
+		html += '<h1>Gallery</h1>';
+		html += imageNamesToTags(imageTags).join(''); 
+		//html += ghtml;
+		html += '</body>';
+	return html;	
+}
+
+function serveGallery(req,res){
+		getImageNames(function(err,imageNames){
+			if(err){
+				console.error(err);
+				res.statusCode = 500;
+				res.statusMessage = 'server error';
+				res.end();
+				return;
+			}
+			res.setHeader("Content-Type","text/html");
+			res.end(buildGallery(imageNames));
+		});
+	
+}
 function serveImage(filename,req,res){
 														// readfile will store in ram - cashe so it is easy to accesss
-	fs.readFile('images/' +filename, function(err,body){ // delete the sync part to async it
+	fs.readFile('images' +filename, function(err,body){ // delete the sync part to async it
 	if(err){
 			console.error(err);
-			res.statusCode  = 500;
-			res.statusMessage = "whoops";
+			res.statusCode  = 404;
+			res.statusMessage = "server error";
 			res.end("silly me");
 			return;
 		}
@@ -37,24 +78,17 @@ function serveImage(filename,req,res){
 var server = http.createServer(function(req, res){
 	
 switch(req.url){
+	case "/":
 	case "/gallery":
-		var html = '<!doctype html>';
-		//res.end("Hello.Time is "+ Date.now());
-		var ghtml = imageNames.map(function(fileName){
-			return ' <img src="' + fileName + '">';
-		}).join();	
+		serveGallery(req,res);
 		
-		html += '<head><title>Dishan Gallery</title>';
-		//html += 'Hello.Time is '+ Date.now();
-		html += '<link href="gallery.css" rel="stylesheet" type="text/css" >';
-		html += '</head>'; 
-		html += '<body>';
-		html += '<h1>Gallery</h1>';
-		//html += '<img src="images/ace.jpg" alt="a fishing ace at work" >';
-		html += ghtml;
-		html += '</body>';
-		res.setHeader('content-Type','text/html');
-		res.end(html);
+		//res.end("Hello.Time is "+ Date.now());
+		//var ghtml = imageNames.map(function(fileName){
+		//	return ' <img src="' + fileName + '">';
+		//}).join();	
+		
+	//	res.setHeader('content-Type','text/html');
+		//res.end(html);
 		break;
 	case"/gallery.css":
 		res.setHeader('Content-Tyep','text/css');
@@ -63,22 +97,9 @@ switch(req.url){
 	case "/chess":
 		serveImage('chess.jpg',req,res);
 		break;
-	case"/fern":	
-	case"/fern/":
-	case"/fern.jpg/":
-	case"/fern.jpeg/":
-		serveImage('fern.jpg',req,res);
-		break;
-	case"/ace":
-		serveImage('ace.jpg',req,res);
-		break;
-	case"/mobile":
-		serveImage('mobile.jpg',req,res);
-		break;
 	default:
-		res.statusCode = 404;
-		res.statusMessage = "Not found";
-		res.end();
+		serveImage(req.url,req,res);
+		break;
 	}
 	
 });

d177504a53697e8354a4ecf5f8bf4f2e01962b29
Dishan Nahtiya <dishan@WIN2.CS.KSU.EDU>
2017-02-21 13:09:26 -0600

    last week friday work

--- server.js
+++ server.js
@@ -3,109 +3,175 @@
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+"use strict;"
 
-/**
-*/
-"use strict";
+/* global variables */
+var multipart = require('./multipart');
 var http = require('http');
-var fs =  require('fs');
-var port = 3001;
+var url = require('url');
+var fs = require('fs');
+var template = require('./templates')
+var port = 3000;
 
+/* load cached files */
+var config = JSON.parse(fs.readFileSync('config.json'));
 var stylesheet = fs.readFileSync('gallery.css');
 
-var imageNames = ['/ace','/bubble','/chess','/fern','/mobile'];
-
-function getImageNames(callback){
-	fs.readdir('images',function(err,fileNames){ // synchro function
-		if(err)callback(err,undefined);
-			else callback(false,fileNames);
-	});
-	
+template.loadDir('templates');
+/** @function getImageNames
+ * Retrieves the filenames for all images in the
+ * /images directory and supplies them to the callback.
+ * @param {function} callback - function that takes an
+ * error and array of filenames as parameters
+ */
+function getImageNames(callback) {
+  fs.readdir('images/', function(err, fileNames){
+    if(err) callback(err, undefined);
+    else callback(false, fileNames);
+  });
 }
 
-function imageNamesToTags(fileNames){
-	return fileNames.map(function(fileName){
-			return ' <img src="'+fileName+'" alt="'+fileName+'">';
-	});
-	
-}
-	
-function buildGallery(imageTags){
-	var html = '<!doctype html>';
-		html += '<head><title>Dishan Gallery</title>';
-		//html += 'Hello.Time is '+ Date.now();
-		html += '<link href="gallery.css" rel="stylesheet" type="text/css" >';
-		html += '</head>'; 
-		html += '<body>';
-		html += '<h1>Gallery</h1>';
-		html += imageNamesToTags(imageTags).join(''); 
-		//html += ghtml;
-		html += '</body>';
-	return html;	
+/** @function imageNamesToTags
+ * Helper function that takes an array of image
+ * filenames, and returns an array of HTML img
+ * tags build using those names.
+ * @param {string[]} filenames - the image filenames
+ * @return {string[]} an array of HTML img tags
+ */
+function imageNamesToTags(fileNames) {
+  return fileNames.map(function(fileName) {
+    return `<h1>${fileName}</h1><a href="${fileName}"><img src="${fileName}" class="picture" alt="${fileName}">`;
+  });
 }
 
-function serveGallery(req,res){
-		getImageNames(function(err,imageNames){
-			if(err){
-				console.error(err);
-				res.statusCode = 500;
-				res.statusMessage = 'server error';
-				res.end();
-				return;
-			}
-			res.setHeader("Content-Type","text/html");
-			res.end(buildGallery(imageNames));
-		});
-	
-}
-function serveImage(filename,req,res){
-														// readfile will store in ram - cashe so it is easy to accesss
-	fs.readFile('images' +filename, function(err,body){ // delete the sync part to async it
-	if(err){
-			console.error(err);
-			res.statusCode  = 404;
-			res.statusMessage = "server error";
-			res.end("silly me");
-			return;
-		}
-		
-		res.setHeader("Content-Type","image/jpeg");
-		res.end(body);
+/**
+ * @function buildGallery
+ * A helper function to build an HTML string
+ * of a gallery webpage.
+ * @param {string[]} imageTags - the HTML for the individual
+ * gallery images.
+ */
+function buildGallery(imageTags) {
+	return template.render 
+return template.render('gallery'){// sending the object
+	title : config.title,
+	imagetags : imageNamesToTags.(imageTags).join('')
 	});
-	
-}	
-var server = http.createServer(function(req, res){
-	
-switch(req.url){
-	case "/":
-	case "/gallery":
-		serveGallery(req,res);
-		
-		//res.end("Hello.Time is "+ Date.now());
-		//var ghtml = imageNames.map(function(fileName){
-		//	return ' <img src="' + fileName + '">';
-		//}).join();	
-		
-	//	res.setHeader('content-Type','text/html');
-		//res.end(html);
-		break;
-	case"/gallery.css":
-		res.setHeader('Content-Tyep','text/css');
-		res.end(stylesheet);
-		break;		
-	case "/chess":
-		serveImage('chess.jpg',req,res);
-		break;
-	default:
-		serveImage(req.url,req,res);
-		break;
-	}
-	
+}
+
+
+
+/** @function serveGallery
+ * A function to serve a HTML page representing a
+ * gallery of images.
+ * @param {http.incomingRequest} req - the request object
+ * @param {http.serverResponse} res - the response object
+ */
+function serveGallery(req, res) {
+  getImageNames(function(err, imageNames){
+    if(err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.statusMessage = 'Server error';
+      res.end();
+      return;
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.end(buildGallery(imageNames));
+  });
+}
+
+/** @function serveImage
+ * A function to serve an image file.
+ * @param {string} filename - the filename of the image
+ * to serve.
+ * @param {http.incomingRequest} - the request object
+ * @param {http.serverResponse} - the response object
+ */
+function serveImage(fileName, req, res) {
+  fs.readFile('images/' + decodeURIComponent(fileName), function(err, data){
+    if(err) {
+      console.error(err);
+      res.statusCode = 404;
+      res.statusMessage = "Resource not found";
+      res.end();
+      return;
+    }
+	var res = fileName.split(".");
+    res.setHeader('Content-Type', 'image/*');
+    res.end(data);
+  });
+}
+
+/** @function uploadImage
+ * A function to process an http POST request
+ * containing an image to add to the gallery.
+ * @param {http.incomingRequest} req - the request object
+ * @param {http.serverResponse} res - the response object
+ */
+function uploadImage(req, res) {
+  multipart(req, res, function(req, res) {
+    // make sure an image was uploaded
+    console.log('filename', req.body.filename)
+    if(!req.body.image.filename) {
+      console.error("No file in upload");
+      res.statusCode = 400;
+      res.statusMessage = "No file specified"
+      res.end("No file specified");
+      return;
+    }
+    fs.writeFile('images/' + req.body.image.filename, req.body.image.data, function(err){
+      if(err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.statusMessage = "Server Error";
+        res.end("Server Error");
+        return;
+      }
+      serveGallery(req, res);
+    });
+  });
+}
+
+/** @function handleRequest
+ * A function to determine what to do with
+ * incoming http requests.
+ * @param {http.incomingRequest} req - the incoming request object
+ * @param {http.serverResponse} res - the response object
+ */
+function handleRequest(req, res) {
+  // at most, the url should have two parts -
+  // a resource and a querystring separated by a ?
+  var urlParts = url.parse(req.url);
+
+  if(urlParts.query){
+    var matches = /title=(.+)($|&)/.exec(urlParts.query);
+    if(matches && matches[1]){
+      config.title = decodeURIComponent(matches[1]);
+      fs.writeFile('config.json', JSON.stringify(config));
+    }
+  }
+
+  switch(urlParts.pathname) {
+    case '/':
+    case '/gallery':
+      if(req.method == 'GET') {
+        serveGallery(req, res);
+      } else if(req.method == 'POST') {
+        uploadImage(req, res);
+      }
+      break;
+    case '/gallery.css':
+      res.setHeader('Content-Type', 'text/css');
+      res.end(stylesheet);
+      break;
+    default:
+      serveImage(req.url, req, res);
+  }
+}
+
+/* Create and launch the webserver */
+var server = http.createServer(handleRequest);
+server.listen(port, function(){
+  console.log("Server is listening on port ", port);
 });
-
-server.listen(port,function(){
- console.log("Listening on port" + port)
-});
-
-//localhost:3000  ' port number
